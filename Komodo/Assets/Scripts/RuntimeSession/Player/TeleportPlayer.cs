51586a4daa0fe926dd9153499468f2d801eee53b
bcdang2 <bcdang2@illinois.edu>
2021-03-18 18:06:58 -0500

    created automatic world center detection

--- Komodo/Assets/Scripts/RuntimeSession/Player/TeleportPlayer.cs
+++ Komodo/Assets/Scripts/RuntimeSession/Player/TeleportPlayer.cs
@@ -10,6 +10,12 @@ namespace Komodo.Runtime
     [RequireComponent(typeof(CameraOffset))]
     public class TeleportPlayer : MonoBehaviour
     {
+        public bool useManualHeightOffset = false;
+
+        public string worldCenterTagName = "WorldCenter";
+
+        private Transform worldCenter;
+
         private Transform cameraRootTransform;
 
         //move desktopPlayer
@@ -29,8 +35,12 @@ namespace Komodo.Runtime
         public void Start()
         {
             originalFixedDeltaTime = Time.fixedDeltaTime;
+
             originalHeight = cameraOffset.cameraYOffset;
+
             currentScale = 1;
+
+            SetWorldCenter();
         }
         
         public void Awake()
@@ -51,6 +61,55 @@ namespace Komodo.Runtime
             {
                 desktopCameraTransform = GameObject.FindGameObjectWithTag("DesktopCamera").transform;
             }
+        }
+
+        public void TempTest ()
+        {
+            List<GameObject> allObjects = new List<GameObject>();
+            UnityEngine.SceneManagement.Scene activeScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+            Debug.Log($"world {activeScene.name}");
+            activeScene.GetRootGameObjects( allObjects );
+            foreach(GameObject obj in allObjects) {
+                if(obj.tag == "WorldCenter") {
+                    Debug.Log($"World {obj.name}");
+                }
+            }
+        }
+
+        public void SetWorldCenter ()
+        {
+            TempTest();
+
+            var worldCenters = GameObject.FindGameObjectsWithTag(worldCenterTagName);
+
+            for (int i = 0; i < worldCenters.Length; i += 1) {
+                //Debug.Log($"WORLD {worldCenters[i].name}");
+
+                if (worldCenters[i].name != worldCenter.gameObject.name) {
+
+                    //Debug.Log($"New World Center found: {worldCenters[i].name}");
+
+                    worldCenter = worldCenters[i].transform;
+
+                    return;
+                }
+            }
+
+            if (worldCenters.Length == 0 && worldCenter == null) {
+                //Debug.LogError($"No GameObjects with tag {worldCenterTagName} were found. Generating one with position <0, 0, 0>.");
+
+                var generatedWorldCenter = new GameObject("GeneratedWorldCenter");
+
+                generatedWorldCenter.tag = worldCenterTagName;
+
+                generatedWorldCenter.transform.SetParent(null);
+
+                worldCenter = generatedWorldCenter.transform;
+
+                return;
+            }
+
+            //Debug.Log($"Using existing World Center: {worldCenter.gameObject.name}");
         }
 
         /// <summary>
@@ -77,6 +136,11 @@ namespace Komodo.Runtime
             desktopCameraTransform.position = homePos;//UIManager.Instance.anchorPositionInNewScene.position;//Vector3.up * defaultPlayerInitialHeight;
 
             UpdatePlayerPosition(new Position { pos = homePos });
+        }
+
+        public void SetPlayerPositionToHome2 () 
+        {
+            desktopCameraTransform.position = worldCenter.position;
         }
         
         public void UpdatePlayerPosition(Position newData)
