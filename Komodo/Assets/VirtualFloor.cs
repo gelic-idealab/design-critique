de193ac9af403495c57bb679f130c053ae4d0d66
bcdang2 <bcdang2@illinois.edu>
2021-03-23 06:45:34 -0500

    updated virtual floor and added script to it

--- Komodo/Assets/VirtualFloor.cs
+++ Komodo/Assets/VirtualFloor.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 public class VirtualFloor : MonoBehaviour
 {
-    public void SetPosition(Vector3 position)
+    public void SetXZPosition(Vector3 position)
     {
-        transform.position = position;
+        Vector3 finalPosition = transform.position;
+
+        finalPosition.x = position.x;
+
+        finalPosition.z = position.z;
+
+        transform.position = finalPosition;
+    }
+
+    public void SetXZPosition(Transform otherTransform)
+    {
+        
+        Vector3 finalPosition = otherTransform.position;
+
+        finalPosition.x = otherTransform.position.x;
+
+        finalPosition.z = otherTransform.position.z;
+
+        transform.position = finalPosition;
+    }
+
+    public void SetYPosition(Vector3 position)
+    {
+        Vector3 finalPosition = transform.position;
+
+        finalPosition.y = position.y;
+
+        transform.position = finalPosition;
     }
 }
